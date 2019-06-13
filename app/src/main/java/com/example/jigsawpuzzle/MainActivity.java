299f171bf80edadbba5b9f971bd97030c835dbcd
hexnniliuum <kd.skutnik@gmail.com>
2019-06-13 11:45:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jigsawpuzzle/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.jigsawpuzzle;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AssetManager am = getAssets();
+        try {
+            final String[] files  = am.list("img");
+
+            GridView grid = findViewById(R.id.grid);
+            grid.setAdapter(new ImageAdapter(this));
+            grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(getApplicationContext(), PuzzleActivity.class);
+                    intent.putExtra("assetName", files[i % files.length]);
+                    startActivity(intent);
+                }
+            });
+        } catch (IOException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT);
+        }
+    }
+
+}
